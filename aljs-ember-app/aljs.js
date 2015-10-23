9e1302ee448ff0964def7bdbae0fe0d1c8678c9d
Kevin Beronilla <kevin.beronilla@gmail.com>
2015-10-23 11:00:59 -0500

    Various page updates; hashbang implementation

--- aljs-ember-app/aljs.js
+++ aljs-ember-app/aljs.js
@@ -1,3 +1,44 @@
+// Defining hashbang
+(function () {
+    var get = Ember.get,
+        set = Ember.set;
+
+    Ember.Location.registerImplementation('hashbang', Ember.HashLocation.extend({
+
+        getURL: function () {
+            return get(this, 'location').hash.substr(2);
+        },
+
+        setURL: function (path) {
+            get(this, 'location').hash = "!" + path;
+            set(this, 'lastSetURL', "!" + path);
+        },
+
+        onUpdateURL: function (callback) {
+            var self = this;
+            var guid = Ember.guidFor(this);
+
+            Ember.$(window).bind('hashchange.ember-location-' + guid, function () {
+                Ember.run(function () {
+                    var path = location.hash.substr(2);
+                    if (get(self, 'lastSetURL') === path) {
+                        return;
+                    }
+
+                    set(self, 'lastSetURL', null);
+
+                    callback(location.hash.substr(2));
+                });
+            });
+        },
+
+        formatURL: function (url) {
+            return '#!' + url;
+        }
+
+    }));
+})();
+
 // Kick off Ember
 App = Ember.Application.create({
     rootElement: '#app'
@@ -160,6 +201,6 @@ App.Router.map(function() {
 
 // // This setting disables the detail routing from showing up in the navbar.
 App.Router.reopen( {
-    location: 'none'
+    location: 'hashbang'
 });
 

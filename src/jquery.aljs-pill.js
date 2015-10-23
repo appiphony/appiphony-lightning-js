d87bd8e5116d0c57f18f2fbdcc6b7bf01272db75
Victor Carreon <victor@appiphony.com>
2015-10-23 15:18:39 -0500

    pill jquery

---
+++ src/jquery.aljs-pill.js
@@ -0,0 +1,24 @@
+if (typeof jQuery.aljs === "undefined") { throw new Error("Please include the ALJS initializer file") }
+
+(function($) {    
+    $.fn.pill = function(options) {
+        var settings = $.extend({
+            assetsLocation: $.aljs.assetsLocation
+            // These are the defaults.
+            
+        }, options );
+        
+        if (this.length === 1) {
+            return this.on('click', '[data-aljs-dismiss="pill"]', function(e) {
+                var $pill = $(this).closest('.slds-pill');
+
+                if ($pill.length > 0) {
+                    $pill.trigger('dismiss.aljs.pill');
+                    $pill.remove();
+                }
+            });
+        } else {
+            throw new Error("This plugin can only be run with a selector")
+        }
+    };
+}(jQuery));

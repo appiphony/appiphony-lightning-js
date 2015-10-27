10165e2be97a696d125b40f8fe2e7f4676c6230b
Kevin Beronilla <kevin.beronilla@gmail.com>
2015-10-27 16:21:21 -0500

    Pills docs; custom events updates (past tense for non-animated events)

--- src/jquery.aljs-pill.js
+++ src/jquery.aljs-pill.js
@@ -4,21 +4,20 @@ if (typeof jQuery.aljs === "undefined") { throw new Error("Please include the AL
     $.fn.pill = function(options) {
         var settings = $.extend({
             assetsLocation: $.aljs.assetsLocation
-            // These are the defaults.
-            
+            // These are the defaults.\
         }, options );
         
         if (this.length === 1) {
             return this.on('click', '[data-aljs-dismiss="pill"]', function(e) {
                 var $pill = $(this).closest('.slds-pill');
-
+                
                 if ($pill.length > 0) {
-                    $pill.trigger('dismiss.aljs.pill');
+                    $pill.trigger('dismissed.aljs.pill');
                     $pill.remove();
                 }
             });
         } else {
-            throw new Error("This plugin can only be run with a selector")
+            throw new Error("This plugin can only be run with a selector targeting one container (e.g., 'body')")
         }
     };
 }(jQuery));

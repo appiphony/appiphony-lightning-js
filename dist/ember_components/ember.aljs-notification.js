96035a2030c75624085784267ed756d1aa1f108e
Kevin Beronilla <kevin.beronilla@gmail.com>
2015-12-21 17:30:12 -0600

    Added fade and duration functionality to Ember

--- dist/ember_components/ember.aljs-notification.js
+++ dist/ember_components/ember.aljs-notification.js
@@ -2,9 +2,17 @@ if (typeof _AljsApp === 'undefined') { throw new Error("Please include ember.alj
 
 _AljsApp.AljsNotificationComponent = Ember.Component.extend({
     layoutName: 'components/aljs-notification',
-    attributeBindings: ['toast'],
+    attributeBindings: ['toast', 'visible', 'fadeDuration', 'duration', 'theme'],
+    init : function(){
+        this._super();
+        this.set('visible', this.getWithDefault('visible', false));
+        this.set('fadeDuration', this.getWithDefault('fadeDuration', 0));
+        this.set('duration', this.getWithDefault('duration', 0));
+        this.set('toast', this.getWithDefault('toast', false));
+    },
     didInsertElement: function() {
     	var $el = this.$();
+        this.set('el', $el);
     	var dismissClass = this.get('dismissClass');
 
         this.$().find('.slds-notify').addClass(this.get('class'));
@@ -15,5 +23,27 @@ _AljsApp.AljsNotificationComponent = Ember.Component.extend({
     			$el.addClass('slds-hide');
     		});
     	}
-    }
+    },
+    fadeNotification : function(){
+        var self = this;
+        var visible = !Ember.isEmpty(this.get('visible')) && this.get('visible').toString() == "true";
+        var fadeDuration = parseInt(this.get('fadeDuration'));
+        var duration = parseInt(this.get('duration'));
+        Ember.run.scheduleOnce('afterRender', this, function(){
+            var el = this.$().find('.fader');
+            if(visible){
+                el.fadeIn(fadeDuration, function(){
+                    if(duration > 0){
+                        var timeout = setTimeout(function(){
+                            self.set('visible', false);
+                        }, duration);
+                        self.set('timeout' , timeout);
+                    }
+                })
+            } else {
+                clearTimeout(this.get('timeout'));
+                el.fadeOut(fadeDuration);
+            }
+        });
+    }.observes('visible').on('init')
 });

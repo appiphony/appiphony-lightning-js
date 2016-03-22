7546c97f4868ecebd8b4cf3cdd92d1cb64f733db
Kevin Beronilla <kevin.beronilla@gmail.com>
2016-03-22 09:23:01 -0500

    Added support for nested tabs (https://github.com/appiphony/appiphony-lightning-js/issues/37); Fixed an issue involving focus and keyup events to select tabs with the keyboard

--- public/src/jquery.aljs-tabs.js
+++ public/src/jquery.aljs-tabs.js
@@ -12,8 +12,8 @@ if (typeof jQuery.aljs === "undefined") { throw new Error("Please include the AL
         initTabs: function() {
             // Bind buttons
             var self = this;
-            var $tabButtons = this.$el.find('a[data-aljs-show]');
-            var children = $('.slds-tabs--default__item, .slds-tabs--scoped__item', this.$el);
+            var $tabButtons = this.$el.find('> .slds-tabs--default__nav > .slds-tabs--default__item > .slds-tabs--default__link, > .slds-tabs--scoped__nav > .slds-tabs--scoped__item > .slds-tabs--scoped__link');
+            var children = this.$el.find('> .slds-tabs--default__nav > .slds-tabs--default__item, > .slds-tabs--scoped__nav > .slds-tabs--scoped__item');
             var tabsObj = {
                 self: self,
                 children: children
@@ -36,20 +36,21 @@ if (typeof jQuery.aljs === "undefined") { throw new Error("Please include the AL
             children.keyup(tabsObj, this.processKeypress);
         },
         selectTab: function(tabId) {
-            this.$el.find('.slds-tabs--default__item, .slds-tabs--scoped__item')
+            this.$el.find('> .slds-tabs--default__nav > .slds-tabs--default__item, > .slds-tabs--scoped__nav > .slds-tabs--scoped__item')
                 .removeClass('slds-active')
-                .find('.slds-tabs--default__link, .slds-tabs--scoped__link')
+                .blur()
+                .find('> .slds-tabs--default__link, > .slds-tabs--scoped__link')
                 .attr('tabindex', '-1')
                 .attr('aria-selected', 'false');
-            this.$el.find('.slds-tabs--default__content, .slds-tabs--scoped__content')
+            this.$el.find('> .slds-tabs--default__content, > .slds-tabs--scoped__content')
                 .hide();
             this.$el.find('[data-aljs-show="' + tabId + '"]')
+                .focus()
                 .closest('li')
                 .addClass('slds-active')
-                .find('.slds-tabs--default__link, .slds-tabs--scoped__link')
+                .find('> .slds-tabs--default__link, > .slds-tabs--scoped__link')
                 .attr('tabindex', '0')
-                .attr('aria-selected', 'true')
-                .focus();
+                .attr('aria-selected', 'true');
             this.$el.find('#' + tabId).show()
                 .trigger('shown.aljs.tabcontent'); // Custom aljs event
             
